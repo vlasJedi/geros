6470f3db845e94de4de8fcba23526abe72f5fefc
vlasJedi <azmodei@protonmail.com>
2025-11-02 21:14:20 +0200

    all services can provide responses and fronted by composite with aggregate, further continue with web testing

--- api/src/main/java/vlas/geros/api/recommendation/Recommendation.java
+++ api/src/main/java/vlas/geros/api/recommendation/Recommendation.java
@@ -17,4 +17,43 @@ public class Recommendation {
         serviceAddress = null;
     }
 
+    public Recommendation(
+            int productId,
+            int recommendationId,
+            String author,
+            int rate,
+            String content,
+            String serviceAddress) {
+
+        this.productId = productId;
+        this.recommendationId = recommendationId;
+        this.author = author;
+        this.rate = rate;
+        this.content = content;
+        this.serviceAddress = serviceAddress;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public int getRecommendationId() {
+        return recommendationId;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getServiceAddress() {
+        return serviceAddress;
+    }
 }
--- microservices/product-composite-service/src/main/java/vlas/geros/microservices/core/productcomposite/ProductCompositeServiceApplication.java
+++ microservices/product-composite-service/src/main/java/vlas/geros/microservices/core/productcomposite/ProductCompositeServiceApplication.java
@@ -2,9 +2,17 @@ package vlas.geros.microservices.core.productcomposite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@ComponentScan("vlas.geros")
 public class ProductCompositeServiceApplication {
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductCompositeServiceApplication.class, args);
